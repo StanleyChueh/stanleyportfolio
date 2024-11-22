a55100c0f0bec3e81a12c47fd7a1e1a4b0cb2082
Stanley_Chueh <stanleychueh28@gmail.com>
2024-11-22 16:47:36 +0800

    test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React from "react";
+import Navbar from "./components/Navbar"; // Navigation Bar
+import About from "./components/About";   // About Section
+import Projects from "./components/Projects"; // Projects Section
+import Contact from "./components/Contact"; // Contact Section
 
 function App() {
   return (
     <div>
-      <h1>Stanley Portfolio</h1>
-      <p>This is a test version to debug the build process.</p>
+      {/* Navigation Bar */}
+      <Navbar />
+
+      {/* About Section */}
+      <About />
+
+      {/* Projects Section */}
+      <Projects />
+
+      {/* Contact Section */}
+      <Contact />
+
+      {/* Footer */}
+      <footer style={{ textAlign: "center", padding: "10px", backgroundColor: "#f4f4f4" }}>
+        <p>&copy; {new Date().getFullYear()} Stanley Chueh. All rights reserved.</p>
+      </footer>
     </div>
   );
 }
 
 export default App;
-
